e52f581abc45cf31ce743fba75451f417e650d69
HSR <hectorsilvarobles@gmail.com>
2019-08-30 13:58:33 -0700

    responsive header

--- src/Components/AboutPage/AboutPage.js
+++ src/Components/AboutPage/AboutPage.js
@@ -5,15 +5,33 @@ import AboutSection_3 from './AboutSection_3/AboutSection_3'
 import AboutSection_4 from './AboutSection_4/AboutSection_4'
 import CallToAction from '../CallToAction/CallToAction'
 import Header from '../Header/Header'
+import SideDrawer from '../Header/SideDrawer/SideDrawer'
+import Backdrop from '../Header/SideDrawer/Backdrop/Backdrop'
 
 export default class AboutPage extends Component {
+    state = {
+        sideDrawerOpen: false
+    };
+
+    drawerToggleClickHandler = () => {
+        this.setState((prevstate) => {
+            return {sideDrawerOpen: !prevstate.sideDrawerOpen}
+        })
+    }
+
+    backdropClick = () => {
+        this.setState({sideDrawerOpen: false})
+    }
+
     componentDidMount = () => {
         window.scrollTo(0, 0);
     }
     render() {
         return (
             <div>
-                <Header />
+                <Header drawerClick={this.drawerToggleClickHandler} />
+                <SideDrawer show={this.state.sideDrawerOpen} />
+                {this.state.sideDrawerOpen? <Backdrop click={this.backdropClick} /> : null}
                 <AboutSection />
                 <AboutSection_2 />
                 <AboutSection_3 />
--- src/Components/ContactPage/ContactPage.js
+++ src/Components/ContactPage/ContactPage.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react'
 import ContactSection from './ContactSection/ContactSection'
 import Header from '../Header/Header'
+import SideDrawer from '../Header/SideDrawer/SideDrawer'
+import Backdrop from '../Header/SideDrawer/Backdrop/Backdrop'
 
 export default class ContactPage extends Component {
+    state = {
+        sideDrawerOpen: false
+    };
+
+    drawerToggleClickHandler = () => {
+        this.setState((prevstate) => {
+            return {sideDrawerOpen: !prevstate.sideDrawerOpen}
+        })
+    }
+
+    backdropClick = () => {
+        this.setState({sideDrawerOpen: false})
+    }
     componentDidMount = () => {
         window.scrollTo(0, 0);
     }
     render() {
         return (
             <div>
-                <Header />
+                <Header drawerClick={this.drawerToggleClickHandler} />
+                <SideDrawer show={this.state.sideDrawerOpen} />
+                {this.state.sideDrawerOpen? <Backdrop click={this.backdropClick} /> : null}
                 <ContactSection />
             </div>
         )
