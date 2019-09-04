aa3ef2710eb858dfba412b06c9d0c63110b66fc3
HSR <hectorsilvarobles@gmail.com>
2019-09-04 14:44:52 -0700

    about page is 80% done

--- src/Components/AboutPage/AboutSection_2/AboutSection_2.js
+++ src/Components/AboutPage/AboutSection_2/AboutSection_2.js
@@ -5,7 +5,14 @@ export default class AboutSection_2 extends Component {
     render() {
         return (
             <div className='about-section_2'>
-                <div className='as2-left-side'></div>
+                <div className='as2-left-side'>
+                    <div className='who-we-are'>
+                        <h1>Who We Are</h1>
+                    </div>
+                    <div className='who-we-are-intro'>
+                        <p>Silva-Garcia Handymen Services, LLC. is a local, newly founded, veteran-owned and operated small business offering a variety of quality maintenance and home improvement duties for homeowners and businesses.</p>
+                    </div>
+                </div>
                 <div className='as2-right-side'></div>
                 
             </div>
--- src/Components/CallToAction/CallToAction.js
+++ src/Components/CallToAction/CallToAction.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react'
+import {Redirect} from 'react-router-dom'
 import './calltoaction.css'
 
 export default class CallToAction extends Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            redirect: false
+        }
+    }
+    handleContactPage = ( ) => {
+        this.setState({
+            redirect: true
+        })
+    }
     render() {
         return (
             <div className='call-to-action'>
-                <div className='cta-wording'></div>
-                <div className='cta-button'></div>
+                {this.state.redirect ? <Redirect to='/contact' /> : null}
+                <div className='cta-wording'>
+                    <h1>Let Us Help Complete Your Next Project</h1>
+                </div>
+                <div className='cta-button'>
+                    <button onClick={() => this.handleContactPage()}>Get An Estimate</button>
+                </div>
                 
             </div>
         )
