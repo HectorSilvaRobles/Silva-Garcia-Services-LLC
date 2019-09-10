7046bc702ede42b58ca503f91300a2dfe9cf0622
HSR <hectorsilvarobles@gmail.com>
2019-09-10 12:34:23 -0700

    hero in homepage

--- src/Components/HomePage/HeroBanner/HeroBanner.js
+++ src/Components/HomePage/HeroBanner/HeroBanner.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react'
 import './herobanner.css'
+import tools from '../../../Media/workers.jpg'
 
 export default class HeroBanner extends Component {
     render() {
         return (
             <div className='hero-banner' id='hero'> 
                 <div className='hp-left-hero'>
-                    <div className='hp-left-hero-info'></div>
+                    <div className='hp-left-hero-info'>
+                        <h1>Need Something Done?</h1>
+                        <h3>We Can Do It For You.</h3>
+                        <button>Get An Estimate</button>
+                    </div>
                 </div>
-                <div className='hp-right-hero'></div>
+                <div className='hp-right-hero'>
+                    <img src={tools} />
+                </div>
                 
             </div>
         )
